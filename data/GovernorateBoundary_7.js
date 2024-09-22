c4c520badabd5c54ceaf030849e7d214b516595d
gksz2022 <98141068+gksz2022@users.noreply.github.com>
2024-09-22 17:20:07 +1000

    Delete data directory

--- data/GovernorateBoundary_7.js
+++
@@ -1,1 +0,0 @@
-var json_GovernorateBoundary_7 = {"type":"FeatureCollection","name":"GovernorateBoundary_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- data/Riversandstreams_10.js
+++
@@ -1,1 +0,0 @@
-var json_Riversandstreams_10 = {"type":"FeatureCollection","name":"Riversandstreams","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
